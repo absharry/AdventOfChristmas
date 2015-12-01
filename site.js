10a417d981904fa9d84ab353e1ae1fe9ea419759
Adam Prentice <adam@earthware.co.uk>
2015-12-01 22:17:31 +0000

    slightly refactor js

--- site.js
+++ site.js
@@ -1,37 +1,37 @@
 var day1Puzzle1 = function () {
-    var file = document.getElementById('1-1-file').files[0];
-    var text = "";
     var floorNumber = 0;
-    var reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = function (e) {
-        // browser completed reading file - display it
-        text = e.target.result;
-        for(var i=0; i<text.length; i++){
+
+    fileReader('1-1-file', function (text) {
+        for (var i = 0; i < text.length; i++) {
             text[i] == '(' ? floorNumber++ : floorNumber--;
         }
-        $('#1-1 .answer span').text(floorNumber);
-        $('#1-1 .answer').show();
-    };
-}
+        showAnswer('#1-1', floorNumber);
+    });
+};
 
 var day1Puzzle2 = function () {
-    var file = document.getElementById('1-2-file').files[0];
-    var text = "";
     var floorNumber = 0;
+
+    fileReader('1-2-file', function (text) {
+        var count = 1;
+        while (floorNumber > -1) {
+            text[count] == '(' ? floorNumber++ : floorNumber--;
+            count++;
+        }
+        showAnswer('#1-2', count);
+    });
+};
+
+var fileReader = function (inputElementId, callback) {
+    var file = document.getElementById(inputElementId).files[0];
     var reader = new FileReader();
     reader.readAsText(file);
     reader.onload = function (e) {
-        // browser completed reading file - display it
-        text = e.target.result;
-        for(var i=0; i<text.length; i++){
-            text[i] == '(' ? floorNumber++ : floorNumber--;
-            if(floorNumber == "-1"){
-                $('#1-2 .answer span').text(i+1);
-                $('#1-2 .answer').show(); 
-                break;
-            }
-        }
-        
-    };
-}
+        callback(e.target.result)
+    }
+};
+
+var showAnswer = function (puzzleId, answer) {
+    $(puzzleId + " .answer span").text(answer);
+    $(puzzleId + " .answer").show();
+};
